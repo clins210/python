171dbc7d3b9d942d80c8bc713dedb42a5c368232
clins210 <72818070+clins210@users.noreply.github.com>
2021-01-09 20:26:18 +0800

    Create snake_test.py

---
+++ snake_test.py
@@ -0,0 +1,17 @@
+"""
+2020.01.09
+Snake Game
+Made with PyGame
+"""
+import pygame, sys, time, random, os
+# Initialize pygame and check for errors encountered
+check_errors = pygame.init()
+if check_errors[1] > 0:
+    print('Had {} errors when initialising game, exiting...'.format(check_errors[1]))
+    sys.exit(-1)
+
+# frame, window and model initialization
+frame_size_x = 720
+frame_size_y = 640
+game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
+pygame.display.set_caption('Snake')
